be529cdc11fd5a7cb9b9f574980473bc7f610802
Annabel <antsar@ttu.ee>
2019-10-22 22:13:47 +0300

    keskmise hinde arvutamise funktsiooni update

--- Ann/keskmine_hinne.py
+++ Ann/keskmine_hinne.py
@@ -1,3 +1,22 @@
+# TODO kindlasti peaks saama koodi efektiivsemaks teha, vaata üle
+# TODO kui jõuad tegele visuaalse poolega ka, siis on tore lastele saata :D
+
+
+def get_average_grade(subject_dict, subject):
+    # TODO lõpeta keskmise hinde arvutamine
+    sum = 0
+    if subject == "" or subject == "a":
+        # TODO a tagastab listi listidest
+        grade_list = subject_dict.values()
+
+    print(grade_list)
+    for grade in grade_list:
+        sum += grade
+
+    sum = sum / len(grade_list)
+    return sum
+
+
 print("Welcome to grade sheet generator!")
 
 subject_num = input("How many subjects do you want to insert? ")
@@ -16,15 +35,12 @@ for subject in subject_list:
         grades.append(grade)
 
     subject_dict[subject] = grades
+    grades = []
 
 
-def get_average_grade(grade_list):
-    for grade in grade_list:
-        print(grade)
-        sum += grade
+subject = input("Which subject do you want to get your average grade for (if you inserted only one grade " +
+                "then press enter)? ")
 
-    sum = sum / len(grade_list)
-    return sum
+get_average_grade(subject_dict, subject)
 
 
-print(subject_dict["eng"])

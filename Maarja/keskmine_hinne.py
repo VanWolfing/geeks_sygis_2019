88a057d14bdafffbd7bd338ba0c35e5b060bcf66
Maarja <mahoop@ttu.ee>
2019-11-19 23:37:17 +0200

    keskmine hinne

--- Maarja/keskmine_hinne.py
+++ Maarja/keskmine_hinne.py
@@ -1,15 +1,16 @@
-hinded = []
+grades = []
 more_grades = "jah"
 m = 1
 while more_grades == "jah":
     grade = input(f"Palun sisesta {m}. hinne:")
-    hinded.append(int(grade))
+    grades.append(int(grade))
     m = m + 1
     if input("Kas sul on veel hindeid? Vasta jah/ei: ").lower() != "jah":
         more_grades = "ei"
-print(hinded)
+print(grades)
 sum_of_marks = 0
-for numbers in hinded:
+for numbers in grades:
     sum_of_marks += numbers
-grade = sum_of_marks / len(hinded)
+grade = sum_of_marks / len(grades)
 print(grade)
+
